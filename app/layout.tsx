c82d16ea39fc8e745ff45574c22918db664b91c0
Rajveer3311 <80219496+Rajveer3311@users.noreply.github.com>
2026-03-31 18:03:34 +0530

    Icon Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/SK Logo.jpg',
         media: '(prefers-color-scheme: light)',
       },
       {
